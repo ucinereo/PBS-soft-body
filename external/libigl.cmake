f9949d04454a2998d850193c6677da0fb27c53f2
ucinereo <nicola.studer2000@gmail.com>
2024-12-03 09:45:08 +0100

    Use own libigl fork for static shadows

--- external/libigl.cmake
+++ external/libigl.cmake
@@ -5,7 +5,7 @@ endif()
 include(FetchContent)
 FetchContent_Declare(
     libigl
-    GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG v2.5.0
+    GIT_REPOSITORY https://github.com/ucinereo/libigl.git
+    GIT_TAG v2.5.1e
 )
 FetchContent_MakeAvailable(libigl)
